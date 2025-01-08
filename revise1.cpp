c2af853a0a4a8205de36ef5521563b38f2b81e07
dishant <aryadishant414@gmail.com>
2025-01-08 07:49:49 +0530

    revised example 3 of lec1.cpp

---
+++ revise1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+// example 1
+// ese example 3 hai of lec1.cpp
+class Teacher {
+public:
+    string name;
+    string dept;
+    string subject;
+    double salary;
+
+    void changeDept(string changeDept) {
+        this->dept = changeDept;
+    }
+
+};
+
+int main() {
+    Teacher t1;
+    t1.name = "dishant";
+    t1.dept = "electrical";
+    cout<<"Teacher name is : "<<t1.name<<endl;
+    cout<<"Teacher Department is : "<<t1.dept<<endl;
+
+    t1.changeDept("computer science");
+    cout<<"After dept change the information is : "<<endl;
+    cout<<"Teacher name is : "<<t1.name<<endl;
+    cout<<"Teacher Department is : "<<t1.dept<<endl;    
+
+    return 0;
+}
